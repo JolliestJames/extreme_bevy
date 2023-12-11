3e6885d113b168cbb68b77a49ea4c39d577d01b2
jame <cloudismydog@gmail.com>
2023-12-10 16:19:15 -0800

    add setup and player spawn systems

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 fn main() {
     App::new()
@@ -10,5 +10,25 @@ fn main() {
             }),
             ..default()
         }))
+        .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
+        .add_systems(Startup, (setup, spawn_player))
         .run();
 }
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0., 0.47, 1.),
+            custom_size: Some(Vec2::new(1., 1.)),
+            ..default()
+        },
+        ..default()
+    });
+}
+
