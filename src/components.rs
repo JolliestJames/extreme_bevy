757c7ad415a609177ec6b394c72316441eaac384
jame <cloudismydog@gmail.com>
2023-12-12 13:17:15 -0800

    add BulletReady, Bullet, and MoveDir components

--- src/components.rs
+++ src/components.rs
@@ -5,3 +5,12 @@ pub struct Player {
     pub handle: usize
 }
 
+#[derive(Component, Clone, Copy)]
+pub struct BulletReady(pub bool);
+
+#[derive(Component)]
+pub struct Bullet;
+
+#[derive(Component, Clone, Copy)]
+pub struct MoveDir(pub Vec2);
+
