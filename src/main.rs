9dd6e24f2009806f2716be0b4fa737c5196d3f46
jame <cloudismydog@gmail.com>
2023-12-18 17:45:10 -0800

    transition to roundend state when a character dies

--- src/main.rs
+++ src/main.rs
@@ -344,6 +344,7 @@ fn kill_players(
     mut commands: Commands,
     players: Query<(Entity, &Transform), (With<Player>, Without<Bullet>)>,
     bullets: Query<&Transform, With<Bullet>>,
+    mut next_state: ResMut<NextState<RollbackState>>,
 ) {
     for (player, player_transform) in &players {
         for bullet_transform in &bullets {
@@ -353,6 +354,7 @@ fn kill_players(
             );
             if distance < PLAYER_RADIUS + BULLET_RADIUS {
                 commands.entity(player).despawn_recursive();
+                next_state.set(RollbackState::RoundEnd);
             }
         }
     }
